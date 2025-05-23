baba9074df8d7063c6e28684da376270f78dede6
Pavel Zobov <pavelzobov123@gmail.com>
2025-05-23 08:18:49 -0400

    feat: support remote deployment

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const NEWSAPIKEY = "0e949704756a44d3b9062013c1f40418";
 
 export const baseUrl =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:3001";
+  process.env.NODE_ENV === "production" ? "https://flashpoint.twilightparadox.com" : "http://localhost:3001";
